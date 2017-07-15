a689f093402927e7c03017d4186a72ff791c70a9
Ahmed Fathy <ahmed-aliraqi@gmail.com>
2017-07-15 02:41:47 +0200

    remove model from mamespaces as default

--- config/artisan-scaffolding.php
+++ config/artisan-scaffolding.php
@@ -10,31 +10,31 @@ return [
      * - database/factories/ModelFactory.php
      *
      */
-    'models_default_namespace' => 'App\Models',
+    'models_default_namespace' => 'App',
 
     /**
      * the default namespace for the relations traits.
      *
      */
-    'relations_default_namespace' => 'App\Models\Relations',
+    'relations_default_namespace' => 'App\Relations',
 
     /**
      * the default namespace for the concerns traits.
      *
      */
-    'concerns_default_namespace' => 'App\Models\Concerns',
+    'concerns_default_namespace' => 'App\Concerns',
 
     /**
      * the default namespace for the mutators traits.
      *
      */
-    'mutators_default_namespace' => 'App\Models\Mutators',
+    'mutators_default_namespace' => 'App\Mutators',
 
     /**
      * the default namespace for the scopes traits.
      *
      */
-    'scopes_default_namespace' => 'App\Models\Scopes',
+    'scopes_default_namespace' => 'App\Scopes',
 
     /**
      * the default namespace for the helpers traits.
